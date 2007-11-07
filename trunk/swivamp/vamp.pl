492ec3fffdac1546f26207a2931b1a63455f7ea4
david.pas <david.pas@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2007-11-07 16:55:46 +0000

    I've changed the whole file -7 Nov

--- trunk/swivamp/vamp.pl
+++ trunk/swivamp/vamp.pl
@@ -15,7 +15,9 @@
 		-/swiaudiodata/audiodata
 	*/
 
-:- module(vamp,[vamp_availablePlugin/1]).
+:- module(vamp,[vamp_plugin_system/1
+		vamp_feature_system/1
+		vamp_plugin_for/3]).
 
 
 
@@ -27,38 +29,85 @@
 
 	*/
 
-						/***************************************************
-						********** Quering available Vamp plugins **********
-						***************************************************/
-
 /** 
-	AvailablePlugins(?PluginName): This predicates checks if the Plugin is on the system (PluginKey nomenclature description is library:pluginName)
+	vamp_plugins_sytem/1 This predicates checks if the Plugin is on the system (PluginKey nomenclature description is library:pluginName)
 	This predicate is non-deterministic
 
 	*/
 
-vamp_plugin(PluginKey):-
+vamp_plugin_system(PluginKey):-
 	vmpl_plugins(ListOfPlugins),
 	member(PluginKey, ListOfPlugins).
 
-vmpl_plugin_for(FeatureType, Plugin, Output):-
+/** 
+	vamp_feature_system/1 says if the feature can be computed by the systems (plugin installed or not). IF not, the plugin should be installed. To 
+	know which plugin we need we use vmpl_plugin_for. 
+
+	*/
+vamp_feature_system(FeatureType):-
+	vamp_plugin_for(FeatureType, PluginKey, _),
+	vamp_plugin_system(PluginKey).
+
+/**
+	Definition of the plugin necessary for each feature and the output from which they are obtained. Necessary for the host...
+	This are not system dependent, they are based in general knowledge and defintion of existing plugins. It's a sort of database storing
+	metadata of plugins in prolog for non-deterministic queries
+	*/
+
+vamp_plugin_for(FeatureType, PluginKey, Output):-
 	FeatureType ='tempo', 
-	Plugin = 'qm-vamp-plugins:qm-tempotracker',
+	PluginKey = 'qm-vamp-plugins:qm-tempotracker',
 	Output = 2.
 
-vmpl_plugin_for(FeatureType, Plugin, Output):-
+vamp_plugin_for(FeatureType, PluginKey, Output):-
 	FeatureType ='beats', 
-	Plugin = 'qm-vamp-plugins:qm-tempotracker',
+	PluginKey = 'qm-vamp-plugins:qm-tempotracker',
 	Output = 0.
 
-vmpl_plugin_for(FeatureType, Plugin, Output):-
-	FeatureType ='beats', 
-	Plugin = 'qm-vamp-plugins:qm-tempotracker',
-	Output = 0.
+vamp_plugin_for(FeatureType, PluginKey, Output):-
+	FeatureType ='detection_fn', 
+	PluginKey = 'qm-vamp-plugins:qm-tempotracker',
+	Output = 1.
 
-vmpl_plugin_for(FeatureType, Plugin, Output):-
+vamp_plugin_for(FeatureType, PluginKey, Output):-
 	FeatureType ='chromagram', 
-	Plugin = 'qm-vamp-plugins:qm-chromagram',
+	PluginKey = 'qm-vamp-plugins:qm-chromagram',
 	Output = 0.
+
+vamp_plugin_for(FeatureType, PluginKey, Output):-
+	FeatureType ='mode', 
+	PluginKey = 'qm-vamp-plugins:qm-keydetector',
+	Output = 1.
+
+vamp_plugin_for(FeatureType, PluginKey, Output):-
+	FeatureType ='tonic', 
+	PluginKey = 'qm-vamp-plugins:qm-keydetector',
+	Output = 0.
+
+vamp_plugin_for(FeatureType, PluginKey, Output):-
+	FeatureType ='key', 
+	PluginKey = 'qm-vamp-plugins:qm-keydetector',
+	Output = 2.
+
+vamp_plugin_for(FeatureType, PluginKey, Output):-
+	FeatureType ='constantq', 
+	PluginKey = 'qm-vamp-plugins:qm-constantq',
+	Output = 0.
+
+vamp_plugin_for(FeatureType, PluginKey, Output):-
+	FeatureType ='tctransform', 
+	PluginKey = 'qm-vamp-plugins:qm-tonalchange',
+	Output = 0.
+
+vamp_plugin_for(FeatureType, PluginKey, Output):-
+	FeatureType ='tcfunction', 
+	PluginKey = 'qm-vamp-plugins:qm-tonalchange',
+	Output = 1.
+
+vamp_plugin_for(FeatureType, PluginKey, Output):-
+	FeatureType ='changepositions', 
+	PluginKey = 'qm-vamp-plugins:qm-tonalchange',
+	Output = 2.
+
 
 
