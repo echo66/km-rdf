7098c4cda340eb3910145eea9ca27aef20eb29ec
yves.raimond <yves.raimond@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2007-11-07 14:32:46 +0000

    David's code

---
+++ trunk/swilib/swiaudioblob.cpp
@@ -0,0 +1,158 @@
+/**
+	Audio data type conversion: AudioVector/AudioBlob
+	David Pastor 2007
+*/
+
+#include "swiaudioblob.h"
+#include <stdio.h>
+#include <iostream>
+
+namespace AudioDataConversion{
+using namespace std;
+
+						/***************************
+						******** Variables *********
+					        ***************************/
+/**
+	This blob is a general BLOB type for audio data like PCM, features...
+	Write callbacks...
+	PROBLEMS IF ATOMIC PREDICATES
+*/
+static PL_blob_t audio_blob = 
+{ PL_BLOB_MAGIC,
+  PL_BLOB_UNIQUE,
+					/* unique representation for audio data*/
+  "audio_blob",
+  NULL,					/* release */
+  NULL,					/* compare */
+  NULL,					/* write */
+  NULL,					/* acquire */
+  NULL,					/* save load to/from .qlf files */
+  NULL,
+};
+
+/**
+	Sizes
+*/
+size_t size_AudioVector = 0;//number of elements of the vector
+size_t size_AudioBlob = 0;//bytes in memory of the blob
+
+
+						/***************************
+						******** Functions *********
+					        ***************************/
+
+/**
+	Initialization: Don't know if necessary
+	-Register Blob type (all the times¿?)
+	-set sizes to 0
+*/
+void
+init_audio_blob(){
+
+	size_AudioVector = 0;
+	size_AudioBlob = 0; 
+	//PL_register_blob_type(&audio_blob);//ref count ++?
+}
+
+/**
+	Returns the size of the current AudioVector object 
+*/
+size_t
+get_AudioVector_size(){
+	
+	return size_AudioVector;
+}
+
+/**
+	Returns the size of the current AudioBlob object 
+*/
+size_t
+get_AudioBlob_size(){
+
+	return size_AudioBlob;
+
+}
+
+/**
+	Unifies the new blob containing the data of AudioVector to the term AudioBlob.
+	Returns the result of the unification
+*/
+int
+vector_to_audio_blob(AudioVector data, AudioBlob blob){
+	
+	//it_audio_blob();//ref count ++?
+	size_AudioVector = data.size();
+	float data_array[size_AudioVector];
+	for(size_t j=0; j<size_AudioVector; j++){
+		data_array[j] = data[j];
+	}	
+	size_AudioBlob = sizeof(data_array);
+	return PL_unify_blob(blob, &data_array, size_AudioBlob, &audio_blob);//ref count ++?? Can be collected¿¿??
+}
+
+/**
+	Returns a vector containing the audio data from a term reference. NO TESTED!!
+*/
+AudioVector 
+term_to_audio_vector(term_t t){
+
+	PL_blob_t *type;        //type
+	void *data;		//void pointer to data
+	AudioVector vector;
+	PL_get_blob(t, &data, &size_AudioBlob, &type);
+	cerr<<size_AudioBlob<<endl;
+	if(type != &audio_blob){
+		throw PlException("Not Audio Data Blob");
+	}
+	size_AudioVector = size_AudioBlob/4;
+	float* floats =	(float *)data;//hope it works
+	for(size_t i=0; i<size_AudioVector; i++){
+		vector.push_back(floats[i]);
+	}
+	return vector;
+}
+
+/**
+	The same for a prolog atom
+*/
+AudioVector 
+atom_to_audio_vector(atom_t a) { 
+	
+	return term_to_audio_vector(PlTerm(PlAtom(a)));
+}
+
+/**
+	This time the AudioBlob represents a blob containing a pointer to the AudioVector. Sizes are not important in here....
+*/
+int
+pointer_to_audio_blob(AudioVector *pointer, AudioBlob blob){
+
+	return PL_unify_blob(blob, (void **)&pointer, sizeof(pointer), &audio_blob);	
+}	
+
+/**
+	Gets the pointer to the data. 
+*/
+AudioVector *
+audio_blob_to_pointer(AudioBlob blob){
+
+	PL_blob_t *type;        //type
+	void *data;		//void pointer to data
+	PL_get_blob(blob, &data, &size_AudioBlob, &type);
+	if(type != &audio_blob){
+		throw PlException("Not Audio Data Blob");
+	}
+	return *(AudioVector **)(data);
+}
+
+}
+
+
+
+
+
+
+
+
+
---
+++ trunk/swilib/swiaudioblob.h
@@ -0,0 +1,90 @@
+/**
+	This is a library for general type conversion of vector<float> to PL_blob_t. It has been created in order to store arbitrary large audio data 		objects defined as AudioVector and AudioBlob.
+	David Pastor 2007 for c4dm, Queen Mary, University of London
+	ToDo: garbage-collection debugging
+*/
+
+#ifndef _AUDIO_BLOB_H_
+#define _AUDIO_BLOB_H_
+
+#include <SWI-cpp.h>
+#include <vector>
+
+/** 
+    New namespace: AudioDataConversion
+*/
+namespace AudioDataConversion{
+using namespace std;
+
+						/**********************
+						*******	Types *********
+						**********************/
+
+/**
+	This is a vector containing general audio data: PCM, features...
+	This library should support arbitrary large vectors.
+*/
+typedef vector<float> AudioVector;// we like C++ vectors because we can get the size easily
+
+/**
+	Term reference to an audio blob. This AudioBlob may wrap
+		--binary data as it is stored in memory
+		--pointers to data
+*/
+typedef term_t AudioBlob; //we like PL_blob_t because we can manage large binary data
+
+						/**************************
+						******* Prototypes ********
+						**************************/
+
+/**
+	Wraps the binary audio data into a Blob. Returns the result of PL_unify_blob
+*/
+int
+vector_to_audio_blob(AudioVector, AudioBlob);
+
+/**
+	Gets the audio data from the Blob (passed as term)
+*/
+AudioVector 
+term_to_audio_vector(AudioBlob);
+
+/**
+	Gets the audio data from the Blob passed as atom
+*/
+AudioVector 
+atom_to_audio_vector(atom_t);
+
+/**
+	Wraps a pointer to an AudioVector into an AudioBlob so we can deal with massive large vectors in memory without memory problems
+*/
+int
+pointer_to_audio_blob(AudioVector *, AudioBlob);
+
+/**
+	Retrieves a pointer to an AudioVector stored in an AudioBlob
+*/
+AudioVector *
+audio_blob_to_pointer(AudioBlob);
+
+/**
+	Retrieves the size of the AudioVector: number of samples, feature values... This is only meaninful when we wrap binary data itself 
+*/
+size_t
+get_AudioVector_size();
+
+/**
+	Retrieves the size of the AudioBlob: number of bytes in memory of the float* representing the data. Only meaninful when storing binary data 		itself
+*/
+size_t
+get_AudioBlob_size();
+
+/**
+	Initialization of the audio_blob. CHECK THIS OUT FOR BETTER GARBAGE-COLLECTION BEHAVOUR!!
+*/
+void
+init_audio_blob();
+
+}
+#endif
+
