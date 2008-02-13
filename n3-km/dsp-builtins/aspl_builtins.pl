9b30b6dfe1bd772198b771407f445f4992bd73e0
yves.raimond <yves.raimond@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2008-02-13 19:19:36 +0000

    SPARQLing for VAMP results now works

--- n3-km/dsp-builtins/aspl_builtins.pl
+++ n3-km/dsp-builtins/aspl_builtins.pl
@@ -19,8 +19,34 @@
 builtins:builtin('http://purl.org/ontology/dsp/aspl_decode',aspl_builtins:decode).
 
 decode(Af,[literal(Ch),literal(Sr),literal(L),Sigs]) :- %plug uri resolution for Af here
-	aspl_builtins:aspl_decode(Af,Out2),
+	uri(Af,File),
+	format(user_error,'DEBUG: Decoding file ~w\n',[File]),
+	aspl_builtins:aspl_decode(File,Out2),
 	Out2='Signal'(Ch,Sr,L,Sigs).
 
+uri(Af,File) :-
+	handler(Protocol,Pred),
+	atom_concat(Protocol,_,Af),
+	ToCall =.. [Pred,Af,File],
+	ToCall.
 
+
+/**
+ * Declare your custom audio file access predicates here
+ */
+:- multifile handler/2. % hook 
+handler(http,http_uri).
+
+http_uri(Http,File) :-
+	www_form_encode(Http,Key),
+	atom_concat('httpcache/',Key,File),
+	http_open(Http,HttpS,[]),
+	open(File,write,FileS),
+	copy_stream_data(HttpS,FileS),
+	close(FileS),close(HttpS).
+
+handler(file,file_uri).
+
+file_uri(FURI,File) :-
+	atom_concat('file://',File,FURI).
 
