ceb874caf500a505057ee3ed91fdaa509ea2b8b0
david.pas <david.pas@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2008-05-13 10:54:21 +0000

    Progess

--- swiladspa/LADSPAHost.cpp
+++ swiladspa/LADSPAHost.cpp
@@ -5,8 +5,20 @@
 
 #include <LADSPALoader.h>
 
+/*************************************************** VARIABLES *****************************************/
+
+/**
+	One loader for any plugin
+*/
 LADSPALoader::LADSPALoader *l_loader;
 
+/**
+	Map of output. One working plugin?? Do I want to output a whole signal back???
+	extend it to cover input singls or something???
+*/
+std::map<int, std::vector<float>> ladspa_output;
+
+/************************************ PREDICATES ******************************/
 /**
 	Get a LADSPA plugins loader
 */
@@ -26,7 +38,7 @@ PREDICATE(ldpl_plugins, 1)
 
 	PlTerm list;
 	PlTail tail(list);
-	for(int j= 0; j<ladspaList.size(); j++){
+	for(unsigned int j= 0; j<ladspaList.size(); j++){
 
 	tail.append(PlTerm((const char*)ladspaList[j].data()));
 	}
@@ -82,90 +94,288 @@ PREDICATE(ldpl_plugin_library,  2)
 	Ports stuff
 */
 
-PREDICATE(ldpl_plugin_parameter_count,  2)
+PREDICATE(ldpl_plugin_ports_count,  2)
 {
 	//+plugin identifier
 	//-parameters count
 
 	std::string ident((char *)A1);
 
-	long count = l_loader->LADSPALoader::plugin_parameter_count(ident);
-	return A2 = PlTerm(count);
-}
-
-PREDICATE(ldpl_inputAudioPorts_count,  2)
-{
-	//+plugin identifier
-	//-input audio ports count
-
-	std::string ident((char *)A1);
-
-	long count = l_loader->LADSPALoader::plugin_iaudioports_count(ident);
-	return A2 = PlTerm(count);
-}
-
-PREDICATE(ldpl_outputAudioPorts_count,  2)
-{
-	//+plugin identifier
-	//-output audio ports count
-
-	std::string ident((char *)A1);
-
-	long count = l_loader->LADSPALoader::plugin_oaudioports_count(ident);
-	return A2 = PlTerm(count);
-}
-
-PREDICATE(ldpl_outputControlPorts_count,  2)
-{
-	//+plugin identifier
-	//-parameters count
-
-	std::string ident((char *)A1);
-
-	long count = l_loader->LADSPALoader::plugin_ocontrolports_count(ident);
+	long count = l_loader->LADSPALoader::plugin_ports_count(ident);
 	return A2 = PlTerm(count);
 }
 
 /*************************************************
-******** DEALING WITH THE PLUGIN *****************
+******************* PORTS ************************
 *************************************************/
+
+PREDICATE(ldpl_input_audio, 2)
+{
+	//+plugin name
+	//-list of input audio ports (index of it)
+
+	std::string ident((char *)A1);
+	std::vector<int> input = l_loader->LADSPALoader::inputAudio_ports(ident);
+	
+	PlTerm list;
+	PlTail tail(list);
+	for(int j=0; j<input.size();j++){
+		
+		tail.append((long)input[j]);
+	}
+	tail.close();
+	return A2 = list;
+}
+
+PREDICATE(ldpl_output_audio, 2)
+{
+	//+plugin name
+	//-list of output audio ports (index of it)
+
+	std::string ident((char *)A1);
+	std::vector<int> input = l_loader->LADSPALoader::outputAudio_ports(ident);
+	
+	PlTerm list;
+	PlTail tail(list);
+	for(int j=0; j<input.size();j++){
+		
+		tail.append((long)input[j]);
+	}
+	tail.close();
+	return A2 = list;
+}
+
+PREDICATE(ldpl_input_control, 2)
+{
+	//+plugin name
+	//-list of input control ports (index of it)
+
+	std::string ident((char *)A1);
+	std::vector<int> input = l_loader->LADSPALoader::inputControl_ports(ident);
+	
+	PlTerm list;
+	PlTail tail(list);
+	for(int j=0; j<input.size();j++){
+		
+		tail.append((long)input[j]);
+	}
+	tail.close();
+	return A2 = list;
+}
+
+PREDICATE(ldpl_output_control, 2)
+{
+	//+plugin name
+	//-list of output control ports (index of it)
+
+	std::string ident((char *)A1);
+	std::vector<int> input = l_loader->LADSPALoader::inputAudio_ports(ident);
+	
+	PlTerm list;
+	PlTail tail(list);
+	for(int j=0; j<input.size();j++){
+		
+		tail.append((long)input[j]);
+	}
+	tail.close();
+	return A2 = list;
+}
+
+PREDICATE(ldpl_port_name_for, 3)
+{
+	//+plugin name
+	//-index
+	//-name
+
+	std::string ident((char *)A1);
+	std::string name = l_loader->LADSPALoader::port_name(ident, (int)A2);
+
+	return A3 = PlTerm(PlAtom(name.data()));		
+}
+
+/**************************************************
+******** MANAGE THE PLUGIN ************************
+**************************************************/
 
 /**
 	Instantiate plugin
 */
-PREDICATE(ldpl_instantiate_plugin, 3)
+PREDICATE(ldpl_instantiate_plugin, 4)
 {
 	//+ident
 	//+sr
+	//+blockSize
 	//-plugin blob
 
 	std::string ident((char *)A1);
 	unsigned long sr = (unsigned long)(long)A2;
+	size_t bsize = (size_t)(long)A3;
 
-	LADSPA_Handle plugin = l_loader->LADSPALoader::instantiate_plugin(ident, sr);
+	LADSPAPlugin::LADSPAPlugin *plugin = l_loader->LADSPALoader::instantiate(ident, sr, bsize);
 
 	term_t plugin_t;
 	plugin_t = PL_new_term_ref();
-	ldpl_register_plugin(plugin, plugin_t);
+	ldpl_register_plugin(plugin, ident, plugin_t);
 
-	return A3 = PlTerm(plugin_t);
+	return A4 = PlTerm(plugin_t);
+}
+
+/**
+ 	connect ports. Should be done just after instantiation.
+*/
+PREDICATE(ldpl_connect_ports, 1)
+{
+	//+plugin
+	std::string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;
+	ldpl_get_plugin(term_t(A1), plugin, ident);
+
+	l_loader->LADSPALoader::connect_audio_ports(plugin);
+	l_loader->LADSPALoader::set_default_controls(plugin);
+
+	return true;
+
+}
+
+/**
+	Default controls
+*/
+PREDICATE(ldpl_set_default_controls, 1)
+{
+	//+plugin
+	std::string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;
+	ldpl_get_plugin(term_t(A1), plugin, ident);
+	l_loader->LADSPALoader::set_default_controls(plugin);
+	return true;
+}
+
+/**
+	Set a parameter
+*/
+PREDICATE(ldpl_set_parameter, 3)
+{
+	//+plugin
+	//+input control port
+	//+value
+	std::string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;
+	ldpl_get_plugin(term_t(A1), plugin, ident);
+	l_loader->LADSPALoader::set_parameter(plugin, int(A2), (float)(double)A3);
+	return true;
+}
+
+/**
+	Set an output control
+*/
+PREDICATE(ldpl_set_output_control, 3)
+{
+	//+plugin
+	//+output control port
+	//+value
+	std::string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;
+	ldpl_get_plugin(term_t(A1), plugin, ident);
+	l_loader->LADSPALoader::set_output_control(plugin, int(A2), (float)(double)A3);
+	return true;
 }
 
 
+/**
+	Init plugin
+*/
+PREDICATE(ldpl_activate_plugin, 1)
+{
+	//+pluginblob
+	string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;	
+
+	ldpl_get_plugin(term_t(A1), plugin, ident);
+	plugin->LADSPAPlugin::activate();
+
+	std::cerr<<"LADSPA HOST: plugin activated"<<std::endl;
+	return true;
+}
 
 
+/**
+	Run
+*/
+PREDICATE(ldpl_run_plugin, 3)
+{
+	//+List of PCM channels
+	//+pluginblob
+	//+BlockSize
 
+	string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;	
+	ldpl_get_plugin(term_t(A2), plugin, ident);
+	
+	
+	LADSPA_Data **bufs = plugin -> LADSPAPlugin::get_audio_input();
+	//Setting input buffers 
+	if(ldpl_set_input_buffers(term_t(A1), bufs, l_loader->LADSPALoader::inputAudio_ports(ident).size(), (size_t)(long)A3)<0) return false;
 
+	//setting refilling buffers y a tomar por culo!
 
+	plugin->LADSPAPlugin::run((size_t)(long)A3);
+	std::cerr<<"LADSPA HOST: data processed"<<std::endl;
+	return true;
+}
 
+/**
+	deactivate plugin
+*/
+PREDICATE(ldpl_deactivate_plugin, 1)
+{
+	//+pluginblob
+	string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;	
 
+	ldpl_get_plugin(term_t(A1), plugin, ident);
+	plugin->LADSPAPlugin::deactivate();
 
+	std::cerr<<"LADSPA HOST: plugin activated"<<std::endl;
+	return true;
+}
 
+/**
+	cleanup plugin
+*/
+PREDICATE(ldpl_cleanup_plugin, 1)
+{
+	//+pluginblob
+	string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;	
 
+	ldpl_get_plugin(term_t(A1), plugin, ident);
+	plugin->LADSPAPlugin::cleanup();
 
+	std::cerr<<"LADSPA HOST: plugin activated"<<std::endl;
+	return true;
 
+	//CLEAN BUFFERS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+}
 
+/**
+	collect output
+*/
+PREDICATE(ldpl_collect_output, 1)
+{
+	//+plugin
+	string ident;
+	LADSPAPlugin::LADSPAPlugin *plugin;	
 
+	ldpl_get_plugin(term_t(A1), plugin, ident);
+
+	std::vector<int> outputs = l_loader->LADSPALoader::outputAudio_ports(ident);
+
+	for(int j=0; j<outputs.size(); j++){
+
+		ladspa_output<outputs[j]
+	}
+
+}
 
 
 
--- swiladspa/ladspa.pl
+++ swiladspa/ladspa.pl
@@ -1,7 +1,7 @@
 /**
 	LADSPA Module for audio analysis.
 	This module defines a LADSPA host for SWI-Prolog allowing querying plugin meta-data and calling plugin processing. 
-	Input and Output are MO::Signal functors defined in swilib.
+	Input and Output are MO::Signal or MO::Frame functors defined in swilib.
 
 	David Pastor Escuredo 2008, c4dm, Queen Mary University of London
 */
@@ -10,12 +10,56 @@
 		,	ldpl_plugins/1
 		,	ldpl_plugin_maker/2
 		,	ldpl_plugin_library/2
+		,	ldpl_plugin_ports_count/2
+		,	ldpl_input_audio/2
+		,	ldpl_output_audio/2
+		,	ldpl_input_control/2
+		,	ldpl_output_control/2
+		, 	ldpl_port_name_for/3
+		,	ldpl_port_description/2
+		,	ldpl_set_parameter/3
+		,	ldpl_set_output_control/3
+		,	ldpl_instantiate_plugin/4
+		,	ldpl_connect_ports/1
+		,	ldpl_set_default_controls/1
+		,	ldpl_activate_plugin/1
+		,	ldpl_run_plugin/3
+		,	ldpl_deactivate_plugin/1
+		, 	ldpl_cleanup_plugin/1
 		]).
 
 :- style_check(-discontiguous).
 :- load_foreign_library(swiladspa).
 
+:- use_module('../swiaudiodata/audiodata').
+:- use_module('../swiaudiosource/audiosource').
+
 :-ldpl_loader.
+
+/*******************************************************
+*** PATTERNS *******************************************
+*******************************************************/
+
+/* 	ldpl_plugins(-ListOfPluginsInSystem)
+
+	ldpl_plugin_maker(+PluginName, -PluginMaker)
+
+	ldpl_plugin_library(+PluginName, -LibraryOwningThePlugin)
+
+	ldpl_plugin_ports_count(+PluginName, -NumberOfPorts)
+
+	ldpl_input_audio(-PluginName, -ListOfIndexesOfInputAudioPorts) Eg: ldpl_input_audio('AmplifierStereo', [0, 1])
+
+	ldpl_output_audio(-PluginName, -ListOfIndexesOfOutputudioPorts)
+
+	...
+
+	ldpl_instantiate_plugin(+Key, +SampleRate, +BlockSize, -Plugin): Creats a plugin. The blockSize is set at the beginning (I may change this)
+
+	ldpl_connect_ports(+Plugin): The host connects the plugin to the specific ports
+
+	ldpl_run_plugin(+ListOfData, +Plugin, +BlockSize): The data is passed as list of __data_id. Results are collected later from the output port 
+*/
 
 /**
 	Non-deterministic scan of plugins in system
@@ -24,3 +68,44 @@
 ldpl_plugin_system(Plugin):-
 	ldpl_plugins(List),
 	member(Plugin, List).
+
+/**
+	Returns a functor 'LadspaPort'(Type, Index, Name)
+
+	ldpl_port_description(?PluginName, 'LadpsaPort'(?Type, ?Index, -Name))
+*/
+ldpl_port_description(Name, 'LadspaPort'(T, I, N)):-
+	ldpl_plugin_system(Name),
+	ldpl_input_audio(Name, InAu),
+	ldpl_output_audio(Name, OutAu),
+	ldpl_input_control(Name, InC),
+	ldpl_output_control(Name, OutC),
+	(is_inAu(T, InAu, I);is_outAu(T, OutAu, I);is_inC(T, InC, I);is_outC(T, OutC, I);fail),
+	ldpl_port_name_for(Name, I, N).	
+is_inAu(T, InAu, I):-
+	member(I, InAu),
+	T = 'InputAudio'.
+is_outAu(T, OutAu, I):-
+	member(I, OutAu),
+	T = 'OutputAudio'.
+is_inC(T, InC, I):-
+	member(I, InC),
+	T = 'InputControl'.
+is_outC(T, OutC, I):-
+	member(I, OutC),
+	T = 'OutputControl'.
+
+/**
+	LADSPA PROCESSOR
+*/
+ldpl_ladspa(Input, Key, Block, Output):-
+	(Input = 'Signal'(C, Sr, L, Signals); Input = 'Frame'(C, Sr, Start, Signals)),
+	ldpl_instantiate_plugin(Key, Sr, Block, Plugin),
+	ldpl_connect_ports(Plugin),
+	ldpl_set_default_controls(Plugin),
+	ldpl_activate_plugin(Plugin).
+	
+	
+	
+	
+
