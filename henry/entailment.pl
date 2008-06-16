30e461d19c95c63f93d87760f3f73146b394b93e
yves.raimond <yves.raimond@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2008-06-16 10:56:58 +0000

    Refactoring Henry's code

---
+++ henry/entailment.pl
@@ -0,0 +1,50 @@
+:- module(entailment,[rdf/3]).
+
+:- use_module(rdf_e).
+
+:- begin_tests(entailment).
+test(list_construction,[]) :-
+	rdf_l(A,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',a),
+	rdf_l(A,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest',B),
+	rdf_l(B,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',b),
+	rdf_l(B,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest',C),
+	rdf_l(C,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',c),
+	rdf_l(C,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest','http://www.w3.org/1999/02/22-rdf-syntax-ns#nil'),!,
+	A = [a,b,c],
+	B = [b,c],
+	C = [c].
+test(list_in1,[]) :-
+	rdf_l(A,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',a),
+        rdf_l(A,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest',B),
+        rdf_l(B,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',b),
+        rdf_l(B,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest',C),
+        rdf_l(C,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',c),
+        rdf_l(C,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest','http://www.w3.org/1999/02/22-rdf-syntax-ns#nil'),
+	rdf_l(D,'http://www.w3.org/2000/10/swap/list#in',A),!,
+	memberchk(D,[a,b,c]).
+test(list_in2,[blocked('The first member/2 creates an infinity of lists => infinite loop. This test should succeed as list_in1.')]) :-
+	rdf_l(D,'http://www.w3.org/2000/10/swap/list#in',A),
+	rdf_l(A,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',a),
+        rdf_l(A,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest',B),
+        rdf_l(B,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',b),
+        rdf_l(B,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest',C),
+        rdf_l(C,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',c),
+        rdf_l(C,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest','http://www.w3.org/1999/02/22-rdf-syntax-ns#nil'),!,
+        memberchk(D,[a,b,c]).
+:- end_tests(entailment).
+
+
+rdf(S,P,O) :-
+	rdf_l(S,P,O).
+rdf(S,P,O) :-
+	rdf_e(S,P,O).
+rdf(S,P,O) :-
+	rdf_b(S,P,O).
+
+
+rdf_l([_],'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest','http://www.w3.org/1999/02/22-rdf-syntax-ns#nil').
+rdf_l([H|_],'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',H).
+rdf_l([_|T],'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest',T) :- T\=='http://www.w3.org/1999/02/22-rdf-syntax-ns#nil'.
+rdf_l(M,'http://www.w3.org/2000/10/swap/list#in',L) :- member(M,L).
+
+
---
+++ henry/n3_to_prolog.pl
@@ -0,0 +1,110 @@
+:- module(n3_to_prolog,[n3_prolog_clause/1,compile_all/0]).
+
+:- use_module(skolemize).
+:- use_module(utils).
+:- consult(namespaces).
+
+:- use_module(library('semweb/rdf_db'),
+              [ rdf_global_id/2,
+                rdf_reachable/3,
+                rdf_has/3,
+                rdf_bnode/1,
+                rdf_is_bnode/1,
+                rdf_subject/1,
+                rdf_equal/2
+              ]).
+:- use_module(library('semweb/rdfs'),
+              [ rdfs_list_to_prolog_list/2 ]).
+
+
+/**
+ * Some unit tests
+ */
+
+/**
+ *
+ */
+
+
+compile_all :-
+	forall(n3_prolog_clause(Clause),(format(user_error,'~w\n',[Clause]),assert(entailment:Clause))).
+
+n3_prolog_clause(':-'(H,B)) :-
+	implies(BodyGraph,HeadGraph),
+	n3_pl(HeadGraph,PLH,B1),
+	n3_pl(BodyGraph,PLB,B2),
+	skolemize(PLH,PLB,Skolem),
+	append(B1,B2,Bindings),
+	merge_bindings(Bindings),
+	member(H,Skolem),
+	list_to_conj(PLB,B).
+	
+n3_pl(Head,PredList,Bindings) :-
+        findall(rdf_e(S,P,O),rdf_db:rdf(S,P,O,Head),Triples),
+        n3_pl2(Triples,PredList,Bindings).
+n3_pl2([],[],[]).
+n3_pl2([rdf_e(_,'http://www.w3.org/1999/02/22-rdf-syntax-ns#first',_)|T],T2,B) :-
+        !,n3_pl2(T,T2,B).
+n3_pl2([rdf_e(_,'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest',_)|T],T2,B) :-
+        !,n3_pl2(T,T2,B).
+n3_pl2([rdf_e(_,'http://www.w3.org/1999/02/22-rdf-syntax-ns#type','http://www.w3.org/1999/02/22-rdf-syntax-ns#List')|T],T2,B) :-
+        !,n3_pl2(T,T2,B).
+n3_pl2([rdf_e(S,P,O)|T],[rdf_e(SS,PP,OO)|T2],Bindings) :-
+        convert_n(S,SS,B1),
+        convert_n(P,PP,B2),
+        convert_n(O,OO,B3),
+        flatten([B1,B2,B3],BH),
+        n3_pl2(T,T2,BT),
+        append(BH,BT,Bindings).
+
+convert_all([],[],[]).
+convert_all([H|T],[H2|T2],Bindings) :-
+        convert_n(H,H2,BH),
+        convert_all(T,T2,BT),
+        append(BH,BT,Bindings).
+convert_n(S,SL,Bindings) :-
+        atomic(S),
+        rdfs_list_to_prolog_list(S,SLT),!,
+        convert_all(SLT,SL,Bindings).
+convert_n(S,T,[binding(S,T)]) :-
+        (universal(S)),!.
+convert_n(S,bnode(T),[binding(S,T)]) :-
+        existential(S),!.
+convert_n(S,S,[]).
+
+
+merge_bindings([]).
+merge_bindings([binding(Node,Term)|T]) :-
+        associate(binding(Node,Term),T),
+        merge_bindings(T).
+associate(_,[]).
+associate(binding(Node,Term),[binding(Node,Term)|T]) :-
+        !,
+        associate(binding(Node,Term),T).
+associate(binding(Node,Term),[_|T]) :-
+        associate(binding(Node,Term),T).
+
+/**
+ * Check the quantification of a variable
+ */
+existential(Node) :-
+        quantification(Node,Q),
+        Q=existential.
+universal(Node) :-
+        quantification(Node,Q),
+        Q=universal.
+quantification(Node,universal) :-
+        Node\=[_|_],
+        rdf_db:rdf_is_bnode(Node),
+        atom_concat(_,'_uqvar',Node),!.
+quantification(Node,existential) :-
+        Node\=[_|_],
+        rdf_db:rdf_is_bnode(Node).
+
+/**
+ * Get back two named graph identifiers linked
+ * together through log:implies
+ */
+implies(Body,Head) :-
+        rdf_db:rdf(Body,log:implies,Head).
+
