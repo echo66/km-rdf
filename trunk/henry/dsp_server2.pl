b71ef412d50c1f4c89f75393d1e880852696e2bc
yves.raimond <yves.raimond@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2008-06-25 17:16:02 +0000

    Scripts

---
+++ trunk/henry/dsp_server2.pl
@@ -0,0 +1,18 @@
+#!/usr/local/bin/pl -g serql_welcome -L128m -G128m -T128m -s
+
+:- [henry].
+
+:- 
+	use_module('dsp-builtins/aspl_builtins'), 
+	use_module('dsp-builtins/vamp_builtins'),
+	use_module('dsp-builtins/similarity_builtins'),
+	use_module('dsp-builtins/musicutils').
+
+source :- 
+	n3_load('dsp-n3/decode.n3'),
+	n3_load('dsp-n3/vamp.n3'),
+	n3_load('dsp-n3/similarity.n3').
+
+:- source.
+:- compile_all.
+
