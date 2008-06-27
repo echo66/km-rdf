26bfb422b8053b031a835748cc7b474faf0e0f34
yves.raimond <yves.raimond@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2008-06-27 16:02:41 +0000

    Shared variable list

--- henry/skolemize.pl
+++ henry/skolemize.pl
@@ -17,23 +17,26 @@ test(skolemize) :-
  * End of unit tests
  */
 
+skolemize(Head,Body,Skolem) :- 
+	free_variables(Body,Vars),
+	skolemize2(Vars,Head,Skolem),!.
 
-skolemize([],_,[]).
-skolemize([rdf_e(S,P,O)|T1],Body,[rdf_e(individual(B,Vars),P,individual(C,Vars))|T2]) :-
+skolemize2(_,[],[]).
+skolemize2(Vars,[rdf_e(S,P,O)|T1],[rdf_e(individual(B,Vars),P,individual(C,Vars))|T2]) :-
 	compound(S),S=bnode(B),((var(B),rdf_bnode(B));true),
 	compound(O),O=bnode(C),((var(C),rdf_bnode(C));true),
-	free_variables(Body,Vars),!,
-	skolemize(T1,Body,T2).
-skolemize([rdf_e(S,P,O)|T1],Body,[rdf_e(individual(B,Vars),P,O)|T2]) :-
+	!,
+	skolemize2(Vars,T1,T2).
+skolemize2(Vars,[rdf_e(S,P,O)|T1],[rdf_e(individual(B,Vars),P,O)|T2]) :-
 	compound(S),S=bnode(B),((var(B),rdf_bnode(B));true),
-	free_variables(Body,Vars),!,
-	skolemize(T1,Body,T2).
-skolemize([rdf_e(S,P,O)|T1],Body,[rdf_e(S,P,individual(B,Vars))|T2]) :-
+	!,
+	skolemize2(Vars,T1,T2).
+skolemize2(Vars,[rdf_e(S,P,O)|T1],[rdf_e(S,P,individual(B,Vars))|T2]) :-
         compound(O),O=bnode(B),((var(B),rdf_bnode(B));true),
-	free_variables(Body,Vars),!,
-        skolemize(T1,Body,T2).
-skolemize([H|T1],Body,[H|T2]) :-
-	skolemize(T1,Body,T2).
+	!,
+        skolemize2(Vars,T1,T2).
+skolemize2(Vars,[H|T1],[H|T2]) :-
+	skolemize2(Vars,T1,T2).
 
 
 
