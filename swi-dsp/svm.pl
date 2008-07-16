7a1c313c8b4a350c542c06960fe5fda0fa0aff4c
david.pas <david.pas@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2008-07-16 15:52:44 +0000

    use case, timbre model (mfcc means and variances) using weka

---
+++ swi-dsp/svm.pl
@@ -0,0 +1,52 @@
+/**
+	Support version machine based genre classifier using vamp features
+
+	Centre for Digital Music, Queen Mary, University of London.
+	Copyright (c) 2008 David Pastor Escuredo and QMUL.
+*/
+
+:-use_module('../swiweka/weka').
+:-use_module('../swiweka/classification').
+:-use_module('../swivamp/vamp_transform').
+:-use_module('../swiaudio/audio').
+:-use_module('../swidata/data').
+
+/**
+	Input is a list of records [song, [mffcmeans], [mfccvars], tag]	
+*/
+
+svm_model(TrainData, TrainSet):-
+	classification:create_classifier('weka.classifiers.functions.SVMreg', [], SVM),
+	Options = ['-C 1.0', '-N 0', '-I weka.classifiers.functions.supportVector.RegSMOImproved', '-L 0.0010', '-W 1', '-P 1.0E-12', '-T 0.0010', '-N 0', '-K weka.classifiers.functions.supportVector.PolyKernel', '-C 250007', '-E 1.0', '-d /home/david/my.model'], 
+	wkpl_set_options(SVM, Options),
+	At = [stringAt('song'), numAt('C0mean'), numAt('C1mean'), numAt('C2mean'), numAt('C3mean'), numAt('C4mean'), numAt('C5mean'), numAt('C6mean'), numAt('C7mean'), numAt('C8mean'), numAt('C9mean'), numAt('C10mean'), numAt('C11mean'), numAt('C12mean'), numAt('C13mean'), numAt('C14mean'), numAt('C15mean'), numAt('C16mean'), numAt('C17mean'), numAt('C18mean'), numAt('C19mean'), numAt('C0var'), numAt('C1var'), numAt('C2var'), numAt('C3var'), numAt('C4var'), numAt('C5var'), numAt('C6var'), numAt('C7var'), numAt('C8var'), numAt('C9var'), numAt('C10var'), numAt('C11var'), numAt('C12var'), numAt('C13var'), numAt('C14var'), numAt('C15var'),numAt('C16var'), numAt('C17var'), numAt('C18var'), numAt('C19var'), stringAt('tag')],
+	SetID = 'genre',	
+	%may need some checks about the class we set here
+	create_classifSet(SetID, At, 41),
+	save_classifSet('genre', '/home/david/my.arff'),
+	classifSets_db('genre', TrainSet),
+	findall(_,jamendo_train(TrainData, TrainSet),_),
+	wkpl_write_arff('/home/david/train.arff', TrainSet),
+	weka:wkpl_build_classifier(TrainSet, SVM).
+	
+	
+jamendo_train(TrainData, TrainSet):-
+	member(Record, TrainData),
+	Record = [_Song, Lmeans, Lvars, _Tag],
+	is_list(Lmeans),
+	is_list(Lvars),
+	length(Lmeans, 20),
+	length(Lvars, 20),
+	flatten(Record, Instance),	
+	weka:wkpl_add_instance(TrainSet, Instance).
+	
+:-Song = '/home/david/Repository/Test.mp3',
+	decode(Song, X), 
+	mix_stereo(X, Y),
+	transform(Y, 'libqm-vamp-plugins', 'qm-similarity', [means, variances], L), 
+	L = [[feature(_, _, D1)], [feature(_, _, D2)]],
+	blob_list(D1, L1),
+	blob_list(D2, L2),
+	svm_model([[Song, L1, L2, 'britpop']], P).
+	
+	
