cd7193d93163a04fe7e72ea9dd95ec644723510c
david.pas <david.pas@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2008-07-24 19:23:02 +0000

    Vamp module

--- trunk/swivamp/vamp.pl
+++ trunk/swivamp/vamp.pl
@@ -13,10 +13,15 @@
 
 
 	Centre for Digital Music, Queen Mary, University of London.
-	Copyright (c) 2008 David Pastor Escuredo and QMUL.
+	Copyright (C) 2007 David Pastor Escuredo and QMUL.
+
+	This program is free software: you can redistribute it and/or modify
+  	it under the terms of the GNU General Public License as published by
+   	the Free Software Foundation, either version 3 of the License, or
+    	(at your option) any later version.
 
 	@author David Pastor Escuredo	
-	@license GPL
+	@license GPLv3
 	@version 1.0
 */
 
