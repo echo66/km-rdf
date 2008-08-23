721f998ca5c47aeea76186ae6d92e079cebfdaf0
david.pas <david.pas@488ef6d3-9437-0410-97f0-e18aaa0e1eb9>
2008-08-23 16:49:29 +0000

    Fixing the ladspa bug

--- trunk/swiladspa/ladspa_transform.pl
+++ trunk/swiladspa/ladspa_transform.pl
@@ -35,15 +35,17 @@ transform(Signal,Lib,Id,Outputs,FinalOutputs):-
 %	block
 %	outcontrols (List)
 
-transform(Signal, Lib, Id, Outputs, FinalOutputs, Options):-
+transform(Signal, Lib, Id, Outputs, Key, Options):-
 	is_list(Options),
-	Signal = signal(Sr, _Data),
+	Signal = signal(Sr, Data),
 	get_channels(Signal, Ch),
 	plugin_key(Lib, Id, Key),
 	option(block(Block), Options, 2048),
         ldpl_instantiate_plugin(Key, Sr, Block, Plugin),
-	ldpl_connect_ports(Plugin).
-	%((option(parameters(Parameters), Options, _), option(outcontrols(OutCtrl), Options, _), !) ; (ldpl_set_default_controls(Plugin))),
+	ldpl_connect_ports(Plugin),
+	((option(parameters(Parameters), Options, _), option(outcontrols(OutCtrl), Options, _), !) ; (ldpl_set_default_controls(Plugin))),
+	(ldpl_activate_plugin(Plugin) ; true),
+	ldpl_run_plugin_framing(Data, Plugin, 0, Block).
 	
 
 % concats the plugin key
@@ -51,8 +53,30 @@ transform(Signal, Lib, Id, Outputs, FinalOutputs, Options):-
 plugin_key(_Lib, Id, Key):-
 	nonvar(Id),!,
 	atom_concat('ladspa-plugin', :, HalfKey),
-	atom_concat(HalfKey, Id, Key).
+	atom_concat(HalfKey, :, HalfKey2),
+	atom_concat(HalfKey2, Id, Key).
 plugin_key(Lib, Id, _):-
 	var(Lib), var(Id).
 
+% routine for processing frames
+
+ldpl_process_signal(Signal, Plugin, Block, ProcessedSignal):-
+	Signal = signal(_Sr, Data),
+	get_samples_per_channel(Signal, L),
+	findall(_,ldpl_process_frames(Data, Plugin, L, Block), _).
+	
+ldpl_process_frames(Data, Plugin, L, Block):-
+	set_framing(Block, L, _, Start),	
+	ldpl_run_plugin_framing(Data, Plugin, Start, Block).
+
+%another routine relying on swiaudio for framing
+
+ldpl_process_signal2(Signal, Plugin, Block, ProcessedSignal):-
+	get_samples_per_channel(Signal, L),
+	findall(_,ldpl_process(Signal, Plugin, L, Block), _).
+	
+ldpl_process(Signal, Plugin, L, Block):-
+	set_framing(Block, L, _, Start),
+	get_frame(Signal, Start, Block, Frame),	
+	ldpl_run_plugin_framing(Frame, Plugin, Block).
 
